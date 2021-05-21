ea0fb933db07a576052990208d4d7fecbc64065f
Richard Baldauf <rbaldauf@szintezis-net.hu>
2021-05-21 17:37:47 +0200

    End of Section 6

---
+++ client/src/app/members/memberlist/memberlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-memberlist',
+  templateUrl: './memberlist.component.html',
+  styleUrls: ['./memberlist.component.css']
+})
+export class MemberListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
